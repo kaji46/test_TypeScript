6cea982bb63412467f90761a0ce0a7977e3fc818
Naoto Kajimura <naotokajimura@gmail.com>
2021-07-24 10:47:29 +0900

    test finished

---
+++ src/typeList.ts
@@ -0,0 +1,22 @@
+/**TypeScriptの基本の型 */
+let bool: boolean = true;
+
+let num: number = 0;
+
+let str: string = "";
+
+let arr: Array<number> = [0, 1, 2];
+
+let arr2: number[] = [0, 1, 2];
+
+let any1: any = false;
+
+const funcA = (): void => {
+  const test = "TEST";
+};
+
+let null1: null = null;
+
+let undefined1: undefined = undefined;
+
+let obj1: object = {};
